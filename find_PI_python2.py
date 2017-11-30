c92647c43c98a646bf5f78e994c81a0d76346c19
kuari <1109443400@qq.com>
2017-11-30 13:46:50 +0800

    新的局域网查找树莓派ip

--- find_PI_python2.py
+++ find_PI_python2.py
@@ -1,40 +1,61 @@
+#!/usr/bin/python
 # -*- coding:utf8 -*-
 import os
-print ('--------------脚本环境----------------\n' + \
-       '系统    ubuntu\n' + \
-       'python版本    2.7\n' + \
-       '--------------------------------------')
-an = raw_input('请问你的树莓派和你的电脑是否在同一网络内？(y/n)')
-while an != 'y':
-    print ('走点心行不行？！(╬▔＾▔)凸检查下环境再来！')
-    break
+import sys
+
+try:
+    import netifaces
+except ImportError:
+    try:
+        command_to_execute = "sudo pip install netifaces || easy_install netifaces"
+        os.system(command_to_execute)
+    except OSError:
+        print "Can NOT install netifaces, Aborted!"
+        sys.exit(1)
+    import netifaces
+
+print ("环境: Debian系,Python2.7\n")
+    
+lines = os.popen('sudo dpkg -l | grep -i nmap')
+if lines == "":
+    print ('你未安装nmap,将为你安装nmap软件')
+    os.system('sudo apt-get install nmap -y')
+
+routingNicName = netifaces.gateways()['default'][netifaces.AF_INET][1]
+
+for interface in netifaces.interfaces():
+    if interface == routingNicName:
+        try:
+            routingIPAddr = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
+            routingIPNetmask = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['netmask']
+            IPS = routingIPAddr.split(".")
+            IPS[3] = "0"
+            routingIPAddr = ".".join(IPS)
+
+            exchange_mask = lambda mask: sum(bin(int(i)).count('1') \
+                                 for i in mask.split('.'))
+
+            routingIPNetmask = exchange_mask(routingIPNetmask)
+        except KeyError:
+            print ("[Warning!!!] 自动获取ip失败，请手动输入网段与子网掩码!")
+            print ("样例为 : ip 192.168.1.0 mask 25")
+            routingIPAddr = raw_input("网段 :")
+            routingIPNetmask = raw_input("子网掩码 :")
+
+print ("---"*10)
+if routingNicName:
+    print (u"网卡: %s"%routingNicName)
+print (u"IP地址: %s"%routingIPAddr)
+print (u"子网掩码: %s"%routingIPNetmask)
+print ("---"*10)
+
+IP = routingIPAddr + "/" + str(routingIPNetmask)
+lines = os.popen("sudo nmap -sP '%s'| grep Raspber -B 2"%IP).readlines()
+if lines == "":
+    print ("局域网内未发现树莓派信息...")
 else:
-    print (' ')
-    print ('让我们继续吧！ \n' + \
-            '检测树莓派的ip需要nmap工具')
-    if os.system('sudo dpkg -l | grep -i nmap') != 0:
-        print ('#############################################################')
-        print ('你未安装nmap,将为你安装nmap软件')
-        print ('#############################################################')
-        os.system('sudo apt-get install nmap -y')
-    else:
-        print ('#############################################################')
-        print ('你已安装nmap软件,直接进行下一步')
-        print ('#############################################################')
-    print (' ')
-    IP = raw_input("请以'192.168.1.0/24'的格式输入你的网段以及子网掩码：")
-    while IP == None:
-        print ('没有检测到你输入的网段，请确认输入！')
-        break
-    else:
-        print (' ')
-        print ("ok,你的输入为：'%s' \n"%IP + \
-               '###################树莓派的信息如下##########################\n' + \
-               ' ')
-        if os.system("sudo nmap -sP '%s'| grep Raspber -B 2"%IP) != 0:
-            print '[Warning]find None !!!'
-        else:
-            pass
-        print (' ')
-        print ('#############################################################\n' + \
-               ' ')
+    lines = lines.readlines()
+    print ("#"*30)
+    for line in lines:
+        print line
+    print ("#"*30) 
